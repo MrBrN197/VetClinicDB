26403e903c3f475522184d05dba5b18fa1d9cec3
brianorora <brianorora@gmail.com>
2021-11-30 14:45:54 +0300

    Add transaction statements

---
+++ transactions.sql
@@ -0,0 +1,34 @@
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+select * from animals;
+ROLLBACK;
+
+
+BEGIN;
+UPDATE animals 
+SET species = 'digimon'
+WHERE name LIKE '%mon'; 
+UPDATE animals 
+SET species = 'pokemon'
+WHERE species is NULL; 
+select * from animals;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete_by_date;
+UPDATE animals
+SET weight_kg = -1 * weight_kg;
+ROLLBACK TO delete_by_date;
+UPDATE animals
+SET weight_kg = -1 * weight_kg
+WHERE weight_kg < 0;
+SELECT * from animals;
+COMMIT;
